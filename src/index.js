5b0812d9b311e0f0f367dd96613ba518bb1e1e8b
Sávio Mendes <saviomd@gmail.com>
2018-07-26 16:01:21 -0300

    Lesson 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import {decorate, observable} from 'mobx';
+import {observer} from 'mobx-react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const appState = observable({
+  count: 0
+})
+appState.decrement = function() {
+  this.count--;
+}
+appState.increment = function() {
+  this.count++;
+}
+
+const Counter = observer(class Counter extends Component {
+  handleDec = () => {
+    this.props.store.decrement();
+  }
+  handleInc = () => {
+    this.props.store.increment();
+  }
+  render () {
+    return (
+      <div>
+        Counter: {this.props.store.count}<br />
+        <button onClick={this.handleDec}>-</button>
+        <button onClick={this.handleInc}>+</button>
+      </div>
+    )
+  }
+})
+
+decorate(Counter, {
+  count: observable,
+});
+
+ReactDOM.render(<Counter store={appState} />, document.getElementById('root')
+);
